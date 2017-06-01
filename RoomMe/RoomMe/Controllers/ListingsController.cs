b297575b277abf59a163ac768974d49fb90e27d0
Collin McCoy <collinmccoy17@gmail.com>
2017-06-01 11:12:48 -0700

    added injections

---
+++ RoomMe/RoomMe/Controllers/ListingsController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using RoomMe.Infrastructure;
+using RoomMe.Models;
+
+namespace RoomMe.Controllers
+{
+    public class ListingsController : ApiController
+    {
+        private RoomMeDataContext db = new RoomMeDataContext();
+
+        // GET: api/Listings
+        public IQueryable<Listing> GetListings()
+        {
+            return db.Listings;
+        }
+
+        // GET: api/Listings/5
+        [ResponseType(typeof(Listing))]
+        public IHttpActionResult GetListing(int id)
+        {
+            Listing listing = db.Listings.Find(id);
+            if (listing == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(listing);
+        }
+
+        // PUT: api/Listings/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutListing(int id, Listing listing)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != listing.ListingID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(listing).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ListingExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Listings
+        [ResponseType(typeof(Listing))]
+        public IHttpActionResult PostListing(Listing listing)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Listings.Add(listing);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = listing.ListingID }, listing);
+        }
+
+        // DELETE: api/Listings/5
+        [ResponseType(typeof(Listing))]
+        public IHttpActionResult DeleteListing(int id)
+        {
+            Listing listing = db.Listings.Find(id);
+            if (listing == null)
+            {
+                return NotFound();
+            }
+
+            db.Listings.Remove(listing);
+            db.SaveChanges();
+
+            return Ok(listing);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ListingExists(int id)
+        {
+            return db.Listings.Count(e => e.ListingID == id) > 0;
+        }
+    }
+}
