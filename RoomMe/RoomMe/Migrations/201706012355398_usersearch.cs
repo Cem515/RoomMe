c0e59efd2cea282353b49995b4b40b3376554aeb
Collin McCoy <collinmccoy17@gmail.com>
2017-06-01 16:58:01 -0700

    backend revamp

---
+++ RoomMe/RoomMe/Migrations/201706012355398_usersearch.cs
@@ -0,0 +1,16 @@
+namespace RoomMe.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class usersearch : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
