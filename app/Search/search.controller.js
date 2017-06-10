a531e555f76f1421e84fef7c0674cb510aca07a9
Collin McCoy <collinmccoy17@gmail.com>
2017-06-10 10:14:38 -0700

    added username to listing post

--- app/Search/search.controller.js
+++ app/Search/search.controller.js
@@ -1,41 +1,49 @@
-(function(){
+(function () {
     'use strict';
 
-   angular
+    angular
         .module('app')
         .controller('SearchController', SearchController)
 
-   SearchController.$inject = ['$location', 'SearchFactory'];
+    SearchController.$inject = ['$location', 'SearchFactory','UserFactory'];
 
-   function SearchController($location, SearchFactory) {
+    function SearchController($location, SearchFactory,UserFactory) {
         /* jshint validthis:true */
         var SearchCtrl = this;
         SearchCtrl.srchObject = {};
-        SearchCtrl.srchObject.city= "";
-        SearchCtrl.srchObject.minPrice=null;
-        SearchCtrl.srchObject.maxPrice=null;
-        SearchCtrl.srchObject.zipCode=null;
+        SearchCtrl.srchObject.city = "";
+        SearchCtrl.srchObject.minPrice = null;
+        SearchCtrl.srchObject.maxPrice = null;
+        SearchCtrl.srchObject.zipCode = null;
         SearchCtrl.fullListing = false;
         SearchCtrl.showResults = false;
+        var arrayIndex = -1;
 
-       SearchCtrl.search = function (params){
-        SearchFactory
-            .searchListings(params)
-            .then(function (response){
-                matches(response.data)
-                SearchCtrl.showResults = true;
-            }, function (error) {
-                console.log(error);
-            })
+        SearchCtrl.search = function (params) {
+            SearchFactory
+                .searchListings(params)
+                .then(function (response) {
+                    matches(response.data)
+                    SearchCtrl.showResults = true;
+                }, function (error) {
+                    console.log(error);
+                })
         }
 
-      function matches (results) {
-           SearchCtrl.Results = results;
-           console.log(results);
-       }
+        function matches(results) {
+            SearchCtrl.Results = results;
+            arrayIndex++;
+            UserFactory
+                .getUser(results[arrayIndex].userId)
+                .then(function (response) {
+                    SearchCtrl.ListerId = response.userName;
+                }, function (error) {
+                    console.log(error);
+                })
+        }
 
-      SearchCtrl.expand = function() {
-           SearchCtrl.fullListing = !SearchCtrl.fullListing;
-       }
+        SearchCtrl.expand = function () {
+            SearchCtrl.fullListing = !SearchCtrl.fullListing;
+        }
     }
 })();
